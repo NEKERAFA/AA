bfab6cfc70db8ebc8cc8542e9018a2ab6a57e057
lKoi <iago_neosky@hotmail.com>
2017-02-14 13:31:59 +0100

    entrenamiento corregido

--- entrenar.m
+++ entrenar.m
@@ -1,4 +1,4 @@
-function entrenar( nombre)
+function entrenar(nombre)
 
     disp('Empezando entrenamiento');
     bd_datos = load(nombre);
@@ -7,23 +7,24 @@ function entrenar( nombre)
     temp = 'Temp_body';
     
     % Nombre del campo del hypnograma
-    salidas_deseadas = 'Hypnograma';
+    salidas_deseadas = 'Hypnogram';
 
     % Obtener el nombre de los sujetos en la bd
     nombre_sujetos = fieldnames(bd_datos);
-    
-    % Número de marcos
-    n_marcos = length(bd_datos.(nombre_sujetos{1}).(temp));
     
     % Número de estados a clasificar
     n_estados = 2;
     
     % Capas ocultas
-    hiddenSize = [5];
+    hiddenSize = [3];
     
     % Como se recorre la BD
     % Para cada sujeto
-    for sujeto=1:length(nombre_sujetos)
+    for i=1:length(nombre_sujetos)
+        % Número de marcos
+        n_marcos = length(bd_datos.(nombre_sujetos{i}).(temp));
+        sujeto = bd_datos.(nombre_sujetos{i});
+        sujeto.(salidas_deseadas) = ceil(sujeto.(salidas_deseadas)*0.1);
         % Para cada marco
         for marco=1:n_marcos
             Entradas(1, marco) = sujeto.(temp)(marco);
@@ -31,7 +32,7 @@ function entrenar( nombre)
             %Entradas(3, marco) = desv_eeg
             SalidasDeseadas(:,marco) = zeros(1, n_estados);
             estado = sujeto.(salidas_deseadas)(marco);
-            SalidasDeseadas(estado,marco) = 1;
+            SalidasDeseadas(estado+1,marco) = 1;
         end
     end
     
