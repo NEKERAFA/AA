e056d7a13d437b42f076c4f01221a221ff41e19c
lKoi <iago_neosky@hotmail.com>
2017-03-02 17:07:12 +0100

    Cambios en el main para experimentar

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ bd = 'sleep-EDF';
 bd_proc = 'bd_proc';
 
 % Numero de veces que se entrena el clasificador
-n = 15;
+n = 50;
 
 % Tipo de clasificador
 type = 'rna';
@@ -14,8 +14,8 @@ type = 'rna';
 hiddenSize = [6 3];
 
 % Procesamos los datos de la BD de entrada
-disp('Analizando BD de entrada...');
-analizar_bd(bd, bd_proc);
+%disp('Analizando BD de entrada...');
+%analizar_bd(bd, bd_proc);
 
 % Preparamos los patrones para pasarselos despues al clasificador
 disp('Preparando entradas y salidas deseadas...');
@@ -23,7 +23,7 @@ disp('Preparando entradas y salidas deseadas...');
 
 % Fracción de muestras mal clasificadas
 conf = 0;
-
+mejor_conf = 1;
 % Entrenamos el clasificador varias veces
 for i=1:n
     % Dividimos los patrones en conjuntos de entrenamiento y test
@@ -38,8 +38,18 @@ for i=1:n
     [rna, tr] = entrenar(entradas, salidas_deseadas, hiddenSize);
     targets = entradas(:,tr.testInd);
     outputs = rna(targets);
-    conf = conf + confusion(salidas_deseadas(:,tr.testInd), outputs);
+    conf_actual = confusion(salidas_deseadas(:,tr.testInd), outputs);
+    if (conf_actual < mejor_conf)
+        mejor_rna = rna;
+        mejor_target = salidas_deseadas(:,tr.testInd);
+        mejor_outputs = outputs;
+        mejor_conf = conf_actual;
+    end
+    
+    conf = conf + conf_actual;
 end
 
 % Media de las confusiones
 conf = conf/n;
+
+plotconfusion(mejor_target,mejor_outputs);
